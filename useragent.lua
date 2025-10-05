fb4507f0d79ec1bf72857784253434c16014de21
Daniil A Megrabyan <d.megrabyan@cadabra.systems>
2025-10-05 18:02:15 +0300

    New agent

--- useragent.lua
+++ useragent.lua
@@ -10,8 +10,8 @@ function UserAgent.Parse(input)
 		return input
 	end
 	input = input:lower()
-	-- Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 Safari/605.1.15
-	local browser, _ = ngx.re.match(input, [[(firefox|fxios|edg|edga|edgios|chrome|crios|safari|opr|opera|version|msie|trident|brave|vivaldi|curl)\/([\d.]+)]], "jo")
+	-- @example Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 Safari/605.1.15
+	local browser, _ = ngx.re.match(input, [[(firefox|fxios|edg|edga|edgios|chrome|crios|safari|opr|opera|version|msie|trident|brave|vivaldi|stagefright|curl)\/([\d.]+)]], "jo")
 	if not browser or #browser < 2 then
 		return "crn:cc:agent:unknown:"..input
 	elseif browser[1] == "version" then
